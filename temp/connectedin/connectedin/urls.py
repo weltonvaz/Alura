47b8095b58b786d64ca13970d051e3954cdf7a5c
Welton Vaz <weltonvaz@gmail.com>
2016-11-14 18:36:50 -0200

    Casa - Alterações feitas em 14/11/2016

--- temp/connectedin/connectedin/urls.py
+++ temp/connectedin/connectedin/urls.py
@@ -1,9 +1,10 @@
-# arquivo connectedin/connectedin/urls.py
-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'connectedin.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('perfis.urls'))
 )
