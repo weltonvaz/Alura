d10ca4b0c1390eff79bf116ea0ea16eda31723fd
weltonvaz <weltonvaz@gmail.com>
2018-06-14 10:54:20 -0300

    alterando arquivos em 14/06/2018

--- django/connectedin/perfis/views.py
+++ django/connectedin/perfis/views.py
@@ -6,6 +6,16 @@ def index(request):
 	return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
 
 def exibir(request, perfil_id):
-	
+
 	perfil = Perfil.objects.get(id=perfil_id)
 	return render(request, 'perfil.html', { "perfil" : perfil })
+
+def convidar(request, perfil_id):
+
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
+
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=1)
